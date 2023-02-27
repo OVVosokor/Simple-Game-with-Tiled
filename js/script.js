2d86a2493061c8912bee7edd6476515e74c964d1
elephantovv <elephantovv@yandex.ru>
2023-02-27 16:10:09 +0300

    .

--- js/script.js
+++ js/script.js
@@ -38,19 +38,19 @@ function canvasApp()  {
         tileMap = [];
         mapIndex = undefined; //= this.tileMap.length;
         isRun = false;
+        frameIndex = 0;
+        player;
+        isPressKey = false;
 
-        constructor() {
-        
-        }
         init() {
             canvas.width = this.mapCols * 32;
             canvas.height = this.mapRows * 32;
-            //console.log( canvas.width );
             this.isRun = true;
-            this.drawScreen();
+            this.player = new Player( tileSheetOfWalks, 9, 100, 50, 100 );
+           // console.log( this.player );
+            gameLoop();
         }
-
-        drawScreen() {
+        renderMap() {
             if ( this.isRun ) {
             
                 for ( let rowCtr = 0; rowCtr < this.mapRows; rowCtr++ ) {
@@ -58,24 +58,170 @@ function canvasApp()  {
                         if ( this.mapIndex === undefined ) {
                             this.mapIndex = 0;
                         }
+                        /*
+                        this.mapIndex++;
+                        //console.log( this.tileMap.length );
+                        if ( this.mapIndex === this.tileMap.length ) {
+                            this.mapIndex = undefined;
+                        }*/
+                        //console.log( 'mapIndex:', this.mapIndex );
+                        let tileId = this.tileMap[ this.mapIndex ] + this.mapIndexOffset;
+                       // console.log( 'mapIndex:', this.mapIndex, 'tileID:', this.tileMap[ this.mapIndex ] );
+                        let sourceX = Math.floor( tileId % 8 ) * 33;
+                        let sourceY = Math.floor( tileId / 8 ) * 33;
+                        //console.log( sourceX+1, sourceY+1, 'colCtr:', colCtr, 'rowCtr:', rowCtr );
+                        ctx.drawImage( tileSheetOfMap, sourceX + 1, sourceY + 1, 32, 32, colCtr * 32, rowCtr * 32, 32, 32 );
+
                         this.mapIndex++;
                         //console.log( this.tileMap.length );
                         if ( this.mapIndex === this.tileMap.length ) {
                             this.mapIndex = undefined;
                         }
-                        //console.log( 'mapIndex:', this.mapIndex );
-                        let tileId = this.tileMap[ this.mapIndex ] + this.mapIndexOffset;
-                        //console.log( 'mapIndex:', this.mapIndex, 'tileID:', this.tileMap[ this.mapIndex ] );
-                        let sourceX = Math.floor( tileId % 8 ) * 33;
-                        let sourceY = Math.floor( tileId / 8 ) * 33;
-                        //console.log( sourceX+1, sourceY+1, 'colCtr:', colCtr, 'rowCtr:', rowCtr );
-                        ctx.drawImage( tileSheet, sourceX + 1, sourceY + 1, 32, 32, colCtr * 32, rowCtr * 32, 32, 32 );
+
                     }
                 }
+            }
         }
+        renderObj( tileSheet, animFrames, x, y, delay, isIdle ) {
+                let animationFrames = [];
+                frameIndexCounter.delay = delay;
+
+                for ( let i = 0; i < animFrames; i++ ) {
+                    animationFrames.push( i );
+                }
+
+                let sourceX = Math.floor( animationFrames[ this.frameIndex ] % 8 ) * 64;
+                let sourceY = Math.floor( animationFrames[ this.frameIndex ] / 8 ) * 64;
+                
+                ctx.drawImage( tileSheet, sourceX, sourceY, 64, 64, x, y, 64, 64 );
+
+                this.frameIndex = frameIndexCounter.frameIndex;
+                //console.log( this.frameIndex );
+                if ( this.frameIndex === animationFrames.length ) {
+                    this.frameIndex = 0;
+                    frameIndexCounter.frameIndex = 0;
+                }
         }
 
+        drawScreen() {
+            frameRateCounter.countFrames();
+            frameIndexCounter.countFrames();
+            this.player.update();
+            this.renderMap();
+            this.renderObj( tileSheetOfCombatDummy, 8, 50, 50, 100, false );
+
+            this.player.render( 'idle' );
+
+            ctx.font = '20px sans-serif';
+            ctx.textBaseline = 'top';
+            ctx.fillText ( "FPS:" + frameRateCounter.lastFrameCount, 0, 10 ); 
+        }
     }
+
+
+    class FrameRateCounter {
+        constructor( delay ) {
+            this.lastFrameCount = 0;
+            let dateTemp = new Date();
+            this.frameLast = dateTemp.getTime();
+            //delete dateTemp;
+            this.frameCtr = 0;
+            this.frameIndex = 0;
+            this.delay = delay;
+        }
+        countFrames() {
+            let dateTemp =new Date();	
+            this.frameCtr++;
+
+            if ( dateTemp.getTime() >= this.frameLast + this.delay ) {
+                //console.log( "frame event" );
+                this.frameIndex++;
+                //console.log( this.frameIndex );
+                this.lastFrameCount = this.frameCtr;
+                this.frameLast = dateTemp.getTime();
+                this.frameCtr = 0;
+            }
+
+            /*
+            if ( this.frameIndex >= 8 ) {
+                this.frameIndex = 0;
+            }
+            */
+            //delete dateTemp;
+        }
+    }
+    class Player {
+        constructor( tileSheet, animFrames, x, y, delay ) {
+            this.frameIndex = 0;
+            this.tileSheet = tileSheet;
+            this.animFrames = animFrames;
+            this.x = x;
+            this.y = y;
+            this.dx = 0;
+            this.dy = 0;
+            this.delay = delay;
+            this.direction = 'down';
+            this.sourceDY = 128;
+            this.moveMode = 'idle';
+        }
+
+        update() {
+            if ( game.isPressKey ) {
+                //this.moveMode = moveMode;
+                switch ( this.direction ) {
+                    case 'up':
+                        this.sourceDY = 0;
+                        this.dx = 0;
+                        this.dy = -1;
+                        break;
+                    case 'left':
+                        this.sourceDY = 64;
+                        this.dx = -1;
+                        this.dy = 0;
+                        break;
+                    case 'down':
+                        this.sourceDY = 128;
+                        this.dx = 0;
+                        this.dy = 1;
+                        break;
+                    case 'right':
+                        this.sourceDY = 192;
+                        this.dx = 1;
+                        this.dy = 0;
+                        break;
+                }
+                //console.log( this.y );
+                this.x = this.x + this.dx;
+                this.y = this.y + this.dy;
+            }
+        }
+
+        render() {
+            let animationFrames = [];
+            frameIndexCounter.delay = this.delay;
+
+            for ( let i = 0; i < this.animFrames; i++ ) {
+                animationFrames.push( i );
+            }
+
+            let sourceX = Math.floor( animationFrames[ this.frameIndex ] % 8 ) * 64;
+            let sourceY = Math.floor( animationFrames[ this.frameIndex ] / 8 ) * 64;
+            ctx.drawImage( this.tileSheet, sourceX, sourceY + this.sourceDY, 64, 64, this.x, this.y, 64, 64 );
+
+            if ( this.moveMode === 'run' /*|| this.frameIndex !== 0*/ ) {
+                this.frameIndex = frameIndexCounter.frameIndex;
+                //console.log( this.frameIndex );
+                if ( this.frameIndex === animationFrames.length ) {
+                    this.frameIndex = 0;
+                    frameIndexCounter.frameIndex = 0;
+                }
+            }else{
+                this.frameIndex = 0;
+
+            }
+        }
+    }
+
 
     //цикл игры
     function gameLoop() {
@@ -83,7 +229,7 @@ function canvasApp()  {
 
         window.requestAnimationFrame( gameLoop );
     }
-
+    
     //обработчики событий
     function mouseMoveHandler( e ) {
         mouseMoveCoords.x = e.offsetX;
@@ -94,50 +240,110 @@ function canvasApp()  {
             mouseClickCoords.x = e.offsetX;
             mouseClickCoords.y = e.offsetY;
     }
+
+    function mouseKeyDownHandler( e ) {
+        switch ( e.code ) {
+            case 'ArrowDown':
+                //console.log('keyPress down');
+                game.player.direction = 'down';
+                game.player.moveMode = 'run';
+                game.isPressKey = true;
+                //console.log(game.player.x)
+                break;
+            case 'ArrowUp':
+                //console.log('keyPress up');
+                game.player.direction = 'up';
+                game.player.moveMode = 'run';
+                game.isPressKey = true;
+                break;
+            case 'ArrowLeft':
+                //console.log('keyPress left');
+                game.player.direction = 'left';
+                game.player.moveMode = 'run';
+                game.isPressKey = true;
+                break;
+            case 'ArrowRight':
+                //console.log('keyPress right');
+                game.player.direction = 'right';
+                game.player.moveMode = 'run';
+                game.isPressKey = true;
+                break;
+            }
+    }
+    function mouseKeyUpHandler( e ) {
+        switch ( e.code ) {
+            case 'ArrowDown':
+                //console.log('keyUnPress down');
+                game.player.moveMode = 'idle';
+                game.isPressKey = false;
+                break;
+            case 'ArrowUp':
+                //console.log('keyUnPress up');
+                game.player.moveMode = 'idle';
+                game.isPressKey = false;
+                break;
+            case 'ArrowLeft':
+                //console.log('keyUnPress left');
+                game.player.moveMode = 'idle';
+                game.isPressKey = false;
+                break;
+            case 'ArrowRight':
+                //console.log('keyUnPress right');
+                game.player.moveMode = 'idle';
+                game.isPressKey = false;
+                break;
+            }
+    }
+
     //слушатели событий
-        //canvas.addEventListener( 'mousemove', mouseMoveHandler );
-       // canvas.addEventListener( 'mouseup', mouseUpHandler );
+       // window.addEventListener( 'mousemove', mouseMoveHandler );
+        //window.addEventListener( 'mouseup', mouseUpHandler );
+    window.addEventListener( 'keydown', mouseKeyDownHandler );
+    window.addEventListener( 'keyup', mouseKeyUpHandler );
     
-    let tileSheet = new Image();
-    tileSheet.addEventListener( 'load', imageLoad , false );
-    tileSheet.src = "tiles/tmw_desert_spacing.png";
-    let jsonObj = {};
+    const tileSheetOfMap = new Image();
+    tileSheetOfMap.addEventListener( 'load', itemLoaded , false );
+    tileSheetOfMap.src = "tiles/tmw_desert_spacing.png";
+
+    const tileSheetOfCombatDummy = new Image();
+    tileSheetOfCombatDummy.addEventListener( 'load', itemLoaded , false );
+    tileSheetOfCombatDummy.src = "tiles/BODY_animation.png";
+
+    const tileSheetOfWalks = new Image();
+    tileSheetOfWalks.addEventListener( 'load', itemLoaded , false );
+    tileSheetOfWalks.src = "tiles/BODY_male.png";
+
+
     let requestURL = 'tiles/безымянный.json';
     let request = new XMLHttpRequest();
     request.open( 'GET', requestURL );
     request.responseType = 'json';
     request.send();
     
-    request.addEventListener( 'load', jsonLoad, false );
+    request.addEventListener( 'load', itemLoaded, false );
+    let loadCount = 0;
+    const itemsToLoad = 4;
 
-    let arrLoad = [];
+    function itemLoaded() { //page 545
+        loadCount++;
+        if ( loadCount >= itemsToLoad ) {
+            
+            tileSheetOfMap.removeEventListener( 'load', itemLoaded , false );
+            request.removeEventListener( 'load', itemLoaded, false );
 
-    function jsonLoad() {
-        arrLoad.push( 'true' ); 
-        jsonObj = request.response; 
-        game.tileMap = jsonObj.layers[0].data;
-    }
-    function imageLoad() {
-        arrLoad.push( 'true' ); 
-    }
-    //проверяем загрузились или нет картинки через 1 секунду
-    setTimeout( ()=> {
-        if ( arrLoad.length === 2 ) {
+            jsonObj = request.response; 
+            game.tileMap = jsonObj.layers[0].data;
             game.init();
-            //console.log( arrLoad );
-            //gameLoop();
-        }else{
-            console.log("images don't loaded");
         }
-    }, 1000 ); 
-    
+    }
 
 
 
     const game = new Game();
+    const frameRateCounter = new FrameRateCounter(1000);
+    const frameIndexCounter = new FrameRateCounter(100);
 
-    //game.init();
-    gameLoop();
+    //gameLoop();
 
 
 }
