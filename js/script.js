c2ae4f5c8d3485adc1e736547e8e0d3a52cf3aa9
elephantovv <elephantovv@yandex.ru>
2023-02-27 09:49:41 +0300

    .

--- js/script.js
+++ js/script.js
@@ -31,13 +31,56 @@ function canvasApp()  {
         x: 0,
         y: 0
     };
+    class Game {
+        mapIndexOffset =-1;
+        mapRows = 20;
+        mapCols = 30;
+        tileMap = [];
+        mapIndex = undefined; //= this.tileMap.length;
+        isRun = false;
 
+        constructor() {
+        
+        }
+        init() {
+            canvas.width = this.mapCols * 32;
+            canvas.height = this.mapRows * 32;
+            //console.log( canvas.width );
+            this.isRun = true;
+            this.drawScreen();
+        }
+
+        drawScreen() {
+            if ( this.isRun ) {
+            
+                for ( let rowCtr = 0; rowCtr < this.mapRows; rowCtr++ ) {
+                    for ( let colCtr = 0; colCtr < this.mapCols; colCtr++ ) {
+                        if ( this.mapIndex === undefined ) {
+                            this.mapIndex = 0;
+                        }
+                        this.mapIndex++;
+                        //console.log( this.tileMap.length );
+                        if ( this.mapIndex === this.tileMap.length ) {
+                            this.mapIndex = undefined;
+                        }
+                        //console.log( 'mapIndex:', this.mapIndex );
+                        let tileId = this.tileMap[ this.mapIndex ] + this.mapIndexOffset;
+                        //console.log( 'mapIndex:', this.mapIndex, 'tileID:', this.tileMap[ this.mapIndex ] );
+                        let sourceX = Math.floor( tileId % 8 ) * 33;
+                        let sourceY = Math.floor( tileId / 8 ) * 33;
+                        //console.log( sourceX+1, sourceY+1, 'colCtr:', colCtr, 'rowCtr:', rowCtr );
+                        ctx.drawImage( tileSheet, sourceX + 1, sourceY + 1, 32, 32, colCtr * 32, rowCtr * 32, 32, 32 );
+                    }
+                }
+        }
+        }
+
+    }
 
     //цикл игры
     function gameLoop() {
-        //game.drawScreen();
-        ctx.rect( 0,0,200,200 )
-        ctx.fill()
+        game.drawScreen();
+
         window.requestAnimationFrame( gameLoop );
     }
 
@@ -54,8 +97,44 @@ function canvasApp()  {
     //слушатели событий
         //canvas.addEventListener( 'mousemove', mouseMoveHandler );
        // canvas.addEventListener( 'mouseup', mouseUpHandler );
+    
+    let tileSheet = new Image();
+    tileSheet.addEventListener( 'load', imageLoad , false );
+    tileSheet.src = "tiles/tmw_desert_spacing.png";
+    let jsonObj = {};
+    let requestURL = 'tiles/безымянный.json';
+    let request = new XMLHttpRequest();
+    request.open( 'GET', requestURL );
+    request.responseType = 'json';
+    request.send();
+    
+    request.addEventListener( 'load', jsonLoad, false );
 
-    //const game = new Game();
+    let arrLoad = [];
+
+    function jsonLoad() {
+        arrLoad.push( 'true' ); 
+        jsonObj = request.response; 
+        game.tileMap = jsonObj.layers[0].data;
+    }
+    function imageLoad() {
+        arrLoad.push( 'true' ); 
+    }
+    //проверяем загрузились или нет картинки через 1 секунду
+    setTimeout( ()=> {
+        if ( arrLoad.length === 2 ) {
+            game.init();
+            //console.log( arrLoad );
+            //gameLoop();
+        }else{
+            console.log("images don't loaded");
+        }
+    }, 1000 ); 
+    
+
+
+
+    const game = new Game();
 
     //game.init();
     gameLoop();
